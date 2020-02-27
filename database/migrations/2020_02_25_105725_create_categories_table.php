93bf29359b8f8a2740da1885a9e95bf52792ed28
x3mart <45395171+x3mart@users.noreply.github.com>
2020-02-27 14:37:26 +0300

    categories migration timestamps off

--- database/migrations/2020_02_25_105725_create_categories_table.php
+++ database/migrations/2020_02_25_105725_create_categories_table.php
@@ -18,10 +18,10 @@ class CreateCategoriesTable extends Migration
             $table->string('category_name') -> comment('Название категории');
             $table->string('category_alias')->comment('Название категории на латинице');
             $table->text('category_description')->comment('Описание раздела');
-            $table->string('category_image')->nullable();
+            $table->string('category_image')->nullable(true);
             $table->boolean('category_private')->default(false);
-            $table->timestamp('category_created_at')->useCurrent();
-            $table->timestamp('category_updated_at')->useCurrent();
+//            $table->timestamp('category_created_at')->useCurrent();
+//            $table->timestamp('category_updated_at')->useCurrent();
         });
     }
 
